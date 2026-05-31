7cf1936946adc808737d76dad8f086b50d2b602e
Juani Schuhmann <juanischuhmann@gmail.com>
2026-05-31 12:06:59 -0300

    Add dwl and someblocks config

---
+++ .config/dwl/blocks.def.h
@@ -0,0 +1,31 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sb-rmpc",	            0,		10},
+
+	{"", "sb-forecast",	        3600,		0},
+
+	{" ", "sb-wireplumber",    3600,		11},
+
+	{"󰘚 ", "sb-cpu",	        5,		0},
+
+	{"󰔏 ", "sb-cpu-temp",	    5,		0},
+
+	{"󰍛 ", "sb-memory",	        5,		0},
+
+	{"󰋊 ", "sb-disk",	        3600,		0},
+
+	{"", "sb-battery",	        60,		0},
+
+	{"󰥔 ", "sb-datetime",		5,		0},
+
+	{"󰚰 ", "sb-updates",	    3600,		0},
+	/* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
+	/* {"", "date '+%b %d (%a) %I:%M%p'",					0,		10}, */
+};
+
+
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
